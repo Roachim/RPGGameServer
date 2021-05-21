19d8854869c4de2411e4fc50abdbc6579829ecdb
Joachim Grubbe Frank <joac1067@stud.kea.dk>
2021-05-21 12:59:12 +0200

    Deleted former test and set up new test using SharedDatabaseFixture. Still does not work.

---
+++ RPGVideoGame.xTest/AdminServiceTestItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RPGVideoGameAPI.Services;
+using RPGVideoGameLibrary.Models;
+using Xunit;
+
+namespace RPGVideoGame.xTest
+{
+    public class AdminServiceTestItem : IClassFixture<SharedDatabaseFixture>
+    {
+        public SharedDatabaseFixture Fixture { get; }
+        public AdminServiceTestItem(SharedDatabaseFixture fixture) => Fixture = fixture;
+
+        [Fact]
+        private void GetAllItems_Gets_Minimum_1_Item()
+        {
+            using (var transaction = Fixture.Connection.BeginTransaction())
+            {
+                using (var context = Fixture.CreateContext(transaction))
+                {
+                    //arrange
+                    var service = new AdminService(context);
+                    //act
+                    IEnumerable<Object> result;
+                    result = service.GetAllItems().GetAwaiter().GetResult().ToList();
+                    //assert
+                    Assert.NotNull(result.ToList()[0]);
+
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ RPGVideoGame.xTest/SharedDatabaseFixture.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using RPGVideoGameAPI.Services;
+using RPGVideoGameLibrary.Models;
+using Xunit;
+
+namespace RPGVideoGame.xTest
+{
+    public class SharedDatabaseFixture : IDisposable
+    {
+        private static readonly object _lock = new object();
+        private static bool _databaseInitialized;
+
+        public DbConnection Connection;
+
+        public SharedDatabaseFixture()
+        {
+            Connection = new SqlConnection(
+                @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=OnlineRPG;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            Seed();
+
+            Connection.Open();
+        }
+
+        public OnlineRPGContext CreateContext(DbTransaction transaction = null)
+        {
+            var context =
+                new OnlineRPGContext(new DbContextOptionsBuilder<OnlineRPGContext>().UseSqlServer(Connection).Options);
+
+            if (transaction != null)
+            {
+                context.Database.UseTransaction(transaction);
+            }
+
+            return context;
+
+        }
+
+        /// <summary>
+        /// The seed method instanciates the database and populates it with controlled data.
+        /// </summary>
+        private void Seed()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+
+                    using (var context = new OnlineRPGContext())
+                    {
+                        //We delete the database and recreate it for clean slate
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+
+                        //Add new items
+                        var one = new Item();
+                        one.ItemName = "Potion";
+                        one.Description = "Good for you";
+                        one.Effect = "hels";
+                        one.TypeId = 1;
+
+
+                        context.AddRange(one);
+                        context.SaveChanges();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+
+            
+        }
+
+        public void Dispose() => Connection.Dispose();
+
+    }
+}
